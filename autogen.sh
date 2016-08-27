e417992d0f718fb5383b96e36fa7c62e122fd2e9
Colomban Wendling <ban@herbesfolles.org>
2016-08-27 19:07:55 +0200

    Don't run rst2man in the configure process

--- autogen.sh
+++ autogen.sh
@@ -1,31 +1,6 @@
 #!/bin/sh
 
 ctags_files=`make -f makefiles/list-translator-input.mak`
-man_files=man/*.in.rst
-
-#
-# Generate templates for man pages
-# foo.1.in.rst = [rst2man] => foo.1.in => [configure] => foo.1
-#
-rst2man=
-if which rst2man > /dev/null; then
-	rst2man=rst2man
-elif which rst2man.py > /dev/null; then
-	rst2man=rst2man.py
-fi
-
-if test x"${rst2man}" != x; then
-    for i in ${man_files}; do
-	o=${i%.rst}
-	echo "rst2man converting $i to $o"
-	${rst2man} $i $o
-    done
-else
-    for i in ${man_files}; do
-	o=${i%.rst}
-	echo "use pre-converted file: $o"
-    done
-fi
 
 misc/dist-test-cases > makefiles/test-cases.mak && \
     if autoreconf -vfi; then
